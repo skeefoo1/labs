d8072374d751bc91f0a11f7fcecbc1dda8b367a8
@skeefoo <vlad.o.prohorov@gmail.com>
2025-12-18 00:46:05 +0400

    lab4

---
+++ lab4/WpfApp1_Lab7/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApp1_Lab7
+{
+    public partial class MainWindow : Window
+    {
+        RotateTransform rotate = new RotateTransform(0);
+        TranslateTransform move = new TranslateTransform(0, 0);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TransformGroup group = new TransformGroup();
+            group.Children.Add(rotate);
+            group.Children.Add(move);
+
+            rect.RenderTransform = group;
+            rect.RenderTransformOrigin = new Point(0.5, 0.5);
+        }
+
+        private void Rotate_Click(object sender, RoutedEventArgs e)
+        {
+            rotate.Angle += 15;
+        }
+
+        private void Move_Click(object sender, RoutedEventArgs e)
+        {
+            move.X += 20;
+        }
+    }
+}
